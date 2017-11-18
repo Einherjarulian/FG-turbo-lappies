97d24e89766901c7c7239ec027bda5cffe19f2c1
Einherjarulian <33769615+Einherjarulian@users.noreply.github.com>
2017-11-18 12:15:22 -0800

    Update chrome.sh
    Chrome installation script

--- chrome.sh
+++ chrome.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #Installing Chrome repo and software
+#Separate script for installing Chrome
 
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/chrome.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
